50257c350a8f9b3ef77b037bca433586f4aa9514
EMIAOZANG <christopher.w.lu@gmail.com>
2014-12-01 18:09:01 -0500

    Update assignment10.py

--- jl6583/assignment10.py
+++ jl6583/assignment10.py
@@ -13,13 +13,13 @@ def main():
     raw_data = get_data()
     data_viewer = DataInspector(raw_data)
     #4: print the sum of the improvement info
-#     sum_data = data_viewer.gradesum_by_borough()
-#     print sum_data
-#     writeToTextfile(sum_data, 'grade_improvements_by_borough', 'txt')
+    sum_data = data_viewer.gradesum_by_borough()
+    print sum_data
+    writeToTextfile(sum_data, 'grade_improvements_by_borough', 'txt')
     #5: plot the grading data
-#     boroughs = ['STATEN ISLAND','MANHATTAN','BROOKLYN','QUEENS','BRONX','NYC']
-#     for borough in boroughs:
-#         data_viewer.plotdata_by_range(borough)
+    boroughs = ['STATEN ISLAND','MANHATTAN','BROOKLYN','QUEENS','BRONX','NYC']
+    for borough in boroughs:
+        data_viewer.plotdata_by_range(borough)
     #6: find the trends for number of inspections over time
     data_viewer.plot_inspection_frequency()
         
